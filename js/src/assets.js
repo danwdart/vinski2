0feeaa65ca8a9f397626e54b9371ba8046815690
Dan <git@dandart.co.uk>
2017-03-08 20:26:35 +0000

    prepack the assets

--- js/src/assets.js
+++ js/src/assets.js
@@ -1,15 +1,17 @@
 import loadAjax from './ajax';
 import compileProgram from './compile';
 
-const assetsToLoad = {
-    noshadowv: 'shaders/noshadow.v.glsl',
-    noshadowf: 'shaders/noshadow.f.glsl',
-    shadowv: 'shaders/shadow.v.glsl',
-    shadowf: 'shaders/shadow.f.glsl',
-    shadowgenv: 'shaders/shadowgen.v.glsl',
-    shadowgenf: 'shaders/shadowgen.f.glsl',
-    vinski1: 'models/vinski1.json',
-    tunnel: 'models/tunnel.json'/*,
+import noshadowv from '../../shaders/noshadow.v.glsl';
+import noshadowf from '../../shaders/noshadow.f.glsl';
+import shadowv from '../../shaders/shadow.v.glsl';
+import shadowf from '../../shaders/shadow.f.glsl';
+import shadowgenv from '../../shaders/shadowgen.v.glsl';
+import shadowgenf from '../../shaders/shadowgen.f.glsl';
+
+import vinski1 from '../../models/vinski1.json';
+import tunnel from '../../models/tunnel.json';
+
+/*
     player: 'models/sandra.json',
     bob: 'models/bob2.json',
     director: 'models/director2.json',
@@ -17,42 +19,40 @@ const assetsToLoad = {
     cthulhu: 'models/cthulhu.json', // by Amanda Jackson, CC-BY-NC-SA
     handoferis: 'models/handoferis.json', // by Archindividual, CC-BY-SA
     fnord: 'models/fnord.json', // by Archindividual, CC-BY-SA
-    discoin: 'models/discoin.json', // by scvalex, CC-BY-SA*/
+    discoin: 'models/discoin.json', // by scvalex, CC-BY-SA
+*/
+
+let assets = {
+    noshadowv,
+    noshadowf,
+    shadowv,
+    shadowf,
+    shadowgenv,
+    shadowgenf,
+    vinski1,
+    tunnel
 };
 
-let arrAssetNames = [],
-    arrAssetFiles = [],
-    assets = {};
 
-for (let key in assetsToLoad) {
-    arrAssetNames.push(key);
-    // Object.values() is not present: somebody screwed with time
-    arrAssetFiles.push(assetsToLoad[key]);
-}
+export default (gl, loading) => new Promise((res, rej) => {
+    loading.remove();
 
-export default (gl, loading) => new Promise((res, rej) => Promise.all(
-    arrAssetFiles.map(a => loadAjax(a))).then((results) => {
-        loading.remove();
-        for (let i in results) {
-            assets[arrAssetNames[i]] = results[i];
-        }
-        let objPrograms = {
-            noshadow: compileProgram(gl, assets.noshadowv, assets.noshadowf)
-            //shadow: compileProgram(r[2], r[3]),
-            //shadowgen: compileProgram(r[4], r[5])
-        },
-        objModels = {
-            vinski1: JSON.parse(assets.vinski1),
-            tunnel: JSON.parse(assets.tunnel)/*,
-            player: JSON.parse(assets.player),
-            bob: JSON.parse(assets.bob),
-            director: JSON.parse(assets.director),
-            platform: JSON.parse(assets.platform),
-            cthulhu: JSON.parse(assets.cthulhu),
-            handoferis: JSON.parse(assets.handoferis),
-            fnord: JSON.parse(assets.fnord),
-            discoin: JSON.parse(assets.discoin)*/
-        };
-        res([objPrograms, objModels]);
-    })
-);
+    let objPrograms = {
+        noshadow: compileProgram(gl, assets.noshadowv, assets.noshadowf)
+        //shadow: compileProgram(r[2], r[3]),
+        //shadowgen: compileProgram(r[4], r[5])
+    },
+    objModels = {
+        vinski1: assets.vinski1,
+        tunnel: assets.tunnel/*,
+        player: JSON.parse(assets.player),
+        bob: JSON.parse(assets.bob),
+        director: JSON.parse(assets.director),
+        platform: JSON.parse(assets.platform),
+        cthulhu: JSON.parse(assets.cthulhu),
+        handoferis: JSON.parse(assets.handoferis),
+        fnord: JSON.parse(assets.fnord),
+        discoin: JSON.parse(assets.discoin)*/
+    };
+    res([objPrograms, objModels]);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,8 @@ module.exports = {
            { test: /\.woff2?/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
            { test: /\.ttf/, loader: "file-loader" },
            { test: /\.eot/, loader: "file-loader" },
-           { test: /\.svg/, loader: "file-loader" }
+           { test: /\.svg/, loader: "file-loader" },
+           { test: /\.glsl/, loader: "webpack-glsl-loader" }
        ]
    }
 };
