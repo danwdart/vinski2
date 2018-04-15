6f3fb9915fa2974942d40cd5345548c287021f47
Dan Dart <git@dandart.co.uk>
2018-04-15 16:15:52 +0100

    upgrade to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,32 @@
 module.exports = {
-   context: __dirname+'/js',
-   entry: './src/index.js',
-   output: {
-       path: __dirname+'/js',
-       publicPath: '/js/',
-       filename: "index.js"
-   },
-   module: {
-       loaders: [
-           { test: /\.css$/, loader: "style-loader!css-loader" },
-           { test: /\.eot/, loader: "file-loader" },
-           { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-           { test: /\.jpg/, loader: "file-loader" },
-           { test: /\.glsl/, loader: "webpack-glsl-loader" },
-           { test: /\.mp3/, loader: "file-loader" },
-           { test: /\.svg/, loader: "file-loader" },
-           { test: /\.ttf/, loader: "file-loader" },
-           { test: /\.woff2?/, loader: "url-loader?limit=10000&mimetype=application/font-woff" }
-       ]
-   }
+    context: __dirname+'/js',
+    entry: './src/index.js',
+    output: {
+        path: __dirname+'/js',
+        publicPath: '/js/',
+        filename: "index.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    }
+                ]
+            },
+            { test: /\.eot/, use: "file-loader" },
+            { test: /\.js$/, exclude: /node_modules/, use: "babel-loader" },
+            { test: /\.jpg/, use: "file-loader" },
+            { test: /\.glsl/, use: "webpack-glsl-loader" },
+            { test: /\.mp3/, use: "file-loader" },
+            { test: /\.svg/, use: "file-loader" },
+            { test: /\.ttf/, use: "file-loader" },
+            { test: /\.woff2?/, use: "url-loader?limit=10000&mimetype=application/font-woff" }
+        ]
+    }
 };
